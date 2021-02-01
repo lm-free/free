1ac483671e714558f780da6b9fbed0daa51313d2
free <2462400058@qq.com>
2021-02-01 22:29:20 +0800

    书籍购物增加简单写法

--- pra/书籍购物车案例/main.js
+++ pra/书籍购物车案例/main.js
@@ -54,14 +54,32 @@ const app = new Vue({
 		}
 	},
 	computed: {
-		//普通for循环 为什么用let 大括号作用域
+		//普通for循环 为什么用let 块级作用域 大括号作用域
 		totalPrice () {
+			// 1.普通for循环
+			// let totalPrice = 0
+			// for(let i =0; i<this.books.length;i++){
+			// 	totalPrice += this.books[i].price*this.books[i].count
+			// }
+			// return totalPrice
+
+			//2。for
+			// let totalPrice = 0
+			// for(let i in this.books) {
+			// 	const book = this.books[i]
+			// 	totalPrice += book.price * book.count
+			// }
+			// return totalPrice
+			//3.let
 			let totalPrice = 0
-			for(let i =0; i<this.books.length;i++){
-				totalPrice += this.books[i].price*this.books[i].count
+			for(let item of this.books) {
+				totalPrice += item.price * item.count
 			}
 			return totalPrice
+
+			//reduce
 		}
+
 	},
 	filters: {
 		showPrice (price) {
@@ -69,3 +87,5 @@ const app = new Vue({
 		}
 	}
 })
+
+//函数式编程
